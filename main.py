387a735e47075048063b1dd1b2410d5cf836e395
klyjm <519812206@qq.com>
2019-02-11 23:38:06 +0800

    修改中

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import cv2
 import os
+import math
 
 
 imgpath=input('图像路径：')
-outputpath=input('输出路径')
+outputpath=input('输出路径：')
 if not os.path.exists(outputpath+'\\'+os.path.basename(imgpath)):
     os.makedirs(outputpath+'\\'+os.path.basename(imgpath))
 for file in os.listdir(imgpath):
-    cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    dataarray=cv2.imread(imgpath+'\\'+file, cv2.IMREAD_GRAYSCALE)
+
+    seed=[]
+    seed.append(maxdata)
+    while len(seed)>0:
+        center=seed[0]
+        centerx=math.floor(center/100)
